dec397de91b7c8f7730f51baaca2faa41256257d
Rémy Coutable <rymai@rymai.com>
2010-10-26 23:09:15 +0200

    Add a specific line for Ruby 1.9.2 to ensure we don't have Test::Unit from stdlib loaded but the Test::Unit gem instead.

--- lib/guard/test/runners/default_test_unit_runner.rb
+++ lib/guard/test/runners/default_test_unit_runner.rb
@@ -1,13 +1,14 @@
 require "#{File.dirname(__FILE__)}/../formatter"
+require "#{File.dirname(__FILE__)}/../../test"
+gem 'test-unit' if RUBY_VERSION =~ %r(1\.9\.2) # Thanks Aaron! http://redmine.ruby-lang.org/issues/show/3561
 require 'test/unit'
-require 'test/unit/testcase'
 require 'test/unit/ui/console/testrunner'
 
 # Thanks to Adam Sanderson for the really good starting point:
 # http://endofline.wordpress.com/2008/02/11/a-custom-testrunner-to-scratch-an-itch/
 # 
 # This class inherits from Test::Unit' standard console TestRunner
-# I'm just overriding some callbacks methods to display nice results
+# I'm just overriding some callbacks methods to display some nicer results
 class DefaultGuardTestRunner < Test::Unit::UI::Console::TestRunner
   include Formatter
   
