da8e07a5ed1533a7fc9dbd925d98c4130c3fa083
Rémy Coutable <rymai@rymai.com>
2010-12-14 23:28:47 +0100

    Removed some tests

--- test/succeeding_test.rb
+++ test/succeeding_test.rb
@@ -3,10 +3,6 @@ class SucceedingTest < Test::Unit::TestCase
     assert_equal(true, true)
   end
   def test_succeeding2
-    sleep(1)
-    assert_equal(true, true)
-  end
-  def test_succeeding3
     assert_equal(true, true)
   end
 end
--- test/unit/failing_test.rb
+++ test/unit/failing_test.rb
@@ -1,12 +1,8 @@
 class FailingTest < Test::Unit::TestCase
   def test_failing1
-    sleep(1)
     assert_equal(true, false)
   end
   def test_failing2
-    assert_equal(true, false)
-  end
-  def test_failing3
     assert_equal(true, false)
   end
 end
