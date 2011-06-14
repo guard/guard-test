2821c82551fab2cc5d75366df4e1d5313fd1e91c
Rémy Coutable <rymai@rymai.me>
2011-06-14 20:47:55 +0200

    Bumped version to 0.3.0!

--- lib/guard/test/version.rb
+++ lib/guard/test/version.rb
@@ -1,6 +1,6 @@
 # encoding: utf-8
 module Guard
   module TestVersion
-    VERSION = "0.3.0.rc5" unless defined? TestVersion::VERSION
+    VERSION = "0.3.0" unless defined? TestVersion::VERSION
   end
 end
