c7e08388fbb7fa6ab6b4e5a0911b40ad4d572a71
Rémy Coutable <remy@rymai.me>
2013-04-21 01:19:54 +0200

    Improve code internals

--- lib/guard/test/runner.rb
+++ lib/guard/test/runner.rb
@@ -3,7 +3,9 @@ module Guard
   class Test
     class Runner
 
-      def initialize(options = {})
+      attr_reader :options
+
+      def initialize(opts = {})
         @options = {
           :bundler  => File.exist?("#{Dir.pwd}/Gemfile"),
           :rubygems => false,
@@ -12,31 +14,31 @@ module Guard
           :drb      => false,
           :zeus     => false,
           :cli      => ''
-        }.merge(options)
+        }.merge(opts)
       end
 
-      def run(paths, options = {})
+      def run(paths, opts = {})
         return true if paths.empty?
 
-        ::Guard::UI.info(options[:message] || "Running: #{paths.join(' ')}", :reset => true)
+        ::Guard::UI.info(opts[:message] || "Running: #{paths.join(' ')}", :reset => true)
 
         system(test_unit_command(paths))
       end
 
       def bundler?
         if @bundler.nil?
-          @bundler = @options[:bundler] && !drb? && !zeus?
+          @bundler = options[:bundler] && !drb? && !zeus?
         end
         @bundler
       end
 
       def rubygems?
-        !bundler? && !zeus? && @options[:rubygems]
+        !bundler? && !zeus? && options[:rubygems]
       end
 
       def drb?
         if @drb.nil?
-          @drb = @options[:drb]
+          @drb = options[:drb]
           begin
             require 'spork-testunit'
           rescue LoadError
@@ -48,45 +50,57 @@ module Guard
 
       def zeus?
         if @zeus.nil?
-          @zeus = @options[:zeus]
+          @zeus = options[:zeus]
           ::Guard::UI.info('Using zeus to run the tests') if @zeus
         end
         @zeus
       end
-      
+
     private
 
       def test_unit_command(paths)
-        cmd_parts = []
-        cmd_parts << "rvm #{@options[:rvm].join(',')} exec" unless @options[:rvm].empty?
-        cmd_parts << 'bundle exec' if bundler?
-        cmd_parts << case true
-                     when drb?
-                       'testdrb'
-                     when zeus?
-                       'zeus test'
-                     else
-                       'ruby'
-                     end
-        cmd_parts << Array(@options[:include]).map { |path| "-I#{path}" } unless zeus?
-        cmd_parts << '-r bundler/setup' if bundler?
-        cmd_parts << '-rubygems' if rubygems?
-
-        unless drb? || zeus?
-          cmd_parts << "-r #{File.expand_path("../guard_test_runner", __FILE__)}"
-          cmd_parts << "-e \"%w[#{paths.join(' ')}].each { |p| load p }\""
-        end
-
-        paths.each { |path| cmd_parts << "\"./#{path}\"" }
-
-        unless drb? || zeus?
-          cmd_parts << '--use-color'
-          cmd_parts << '--runner=guard'
-        end
-
-        cmd_parts << @options[:cli]
+        cmd_parts = executables
+        cmd_parts.concat(includes_and_requires(paths))
+        cmd_parts.concat(test_files_list(paths))
+        cmd_parts.concat(command_options)
+        cmd_parts << options[:cli]
 
         cmd_parts.compact.join(' ').strip
+      end
+
+      def executables
+        parts = []
+        parts << "rvm #{options[:rvm].join(',')} exec" unless options[:rvm].empty?
+        parts << 'bundle exec' if bundler?
+        parts << case true
+                 when drb? then 'testdrb'
+                 when zeus? then 'zeus test'
+                 else 'ruby'; end
+      end
+
+      def includes_and_requires(paths)
+        parts = []
+        parts << Array(options[:include]).map { |path| "-I#{path}" } unless zeus?
+        parts << '-r bundler/setup' if bundler?
+        parts << '-rubygems' if rubygems?
+        unless drb? || zeus?
+          parts << "-r #{File.expand_path("../guard_test_runner", __FILE__)}"
+          parts << "-e \"%w[#{paths.join(' ')}].each { |p| load p }\""
+        end
+
+        parts
+      end
+
+      def test_files_list(paths)
+        paths.map { |path| "\"./#{path}\"" }
+      end
+
+      def command_options
+        if drb? || zeus?
+          []
+        else
+          ['--use-color', '--runner=guard']
+        end
       end
 
     end
