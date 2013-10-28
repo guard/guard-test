cfdde3015540c53a8b70e3db1708f912d86074a1
Rémy Coutable <remy@rymai.me>
2013-10-28 22:41:45 +0100

    Bump to 2.0.1

--- spec/lib/guard/test/version_spec.rb
+++ spec/lib/guard/test/version_spec.rb
@@ -5,7 +5,7 @@ describe Guard::TestVersion do
 
   describe "run" do
     it "should be in early development stage" do
-      expect(described_class::VERSION).to eq '2.0.0'
+      expect(described_class::VERSION).to eq '2.0.1'
     end
   end
 
