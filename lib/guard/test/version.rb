991cf99e4c4a3f27d3528745236de9cbb18b775b
Rémy Coutable <remy@rymai.me>
2013-10-30 16:11:08 +0100

    Bump to 2.0.2

--- lib/guard/test/version.rb
+++ lib/guard/test/version.rb
@@ -1,6 +1,6 @@
 # encoding: utf-8
 module Guard
   module TestVersion
-    VERSION = '2.0.1'
+    VERSION = '2.0.2'
   end
 end
--- spec/lib/guard/test/version_spec.rb
+++ spec/lib/guard/test/version_spec.rb
@@ -3,10 +3,8 @@ require 'spec_helper'
 
 describe Guard::TestVersion do
 
-  describe "run" do
-    it "should be in early development stage" do
-      expect(described_class::VERSION).to eq '2.0.1'
-    end
+  it 'has the right version number' do
+    expect(described_class::VERSION).to eq '2.0.2'
   end
 
 end
