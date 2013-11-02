12791c76da2666c695d3247d644e5f63b0c56666
Rémy Coutable <remy@rymai.me>
2013-11-02 18:34:13 +0100

    Bump to 2.0.3

--- spec/lib/guard/test/version_spec.rb
+++ spec/lib/guard/test/version_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe Guard::TestVersion do
 
   it 'has the right version number' do
-    expect(described_class::VERSION).to eq '2.0.2'
+    expect(described_class::VERSION).to eq '2.0.3'
   end
 
 end
